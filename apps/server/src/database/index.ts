be6828eb18c4b2b54105d8b078a1a88645c6623a
CDi <github@cristd.cc>
2024-03-03 00:07:56 +0200

    chore: update db

--- apps/server/src/database/index.ts
+++ apps/server/src/database/index.ts
@@ -1,10 +1,10 @@
 import { Database } from 'bun:sqlite';
 import { drizzle } from 'drizzle-orm/bun-sqlite';
 
-const sqlite = new Database('sqlite.db', { create: true });
+const sqlite = new Database('./src/database/sqlite.db', { create: true });
 const db = drizzle(sqlite);
 
-// Create a table
+// Create a todo table
 const createTodoTable = sqlite.query(`create table  if not exists todo (
 	id integer primary key autoincrement,
 	title text not null,
