7bdfd384ad94c7ddbc007567d37680b5b4815ba1
CDi <github@cristd.cc>
2024-03-03 02:00:18 +0200

    feat: add cors on BE

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,7 +1,16 @@
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 import todo from './todo';
 
+const corsOptions = {
+	origin: 'http://localhost:5173',
+	methods: ['GET', 'POST', 'PUT', 'DELETE'],
+	allowedHeaders: ['Content-Type'],
+};
+
 const app = new Hono();
+
+app.use(cors(corsOptions));
 
 app.get('/', (c) => {
 	// I'm a tea pot response
