ea655aa158b0607610bcc1f6d630a74d12d47dd7
CDi <github@cristd.cc>
2024-03-03 02:00:34 +0200

    chore: fuck this lint error

--- apps/web/.eslintrc.cjs
+++ apps/web/.eslintrc.cjs
@@ -3,13 +3,22 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    'plugin:@tanstack/eslint-plugin-query/recommended'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'vue/multi-word-component-names': [
+      'error',
+      {
+        ignores: ['Button']
+      }
+    ]
   }
 }
